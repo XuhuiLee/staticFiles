d509cb937f4a15784a60353910ea50289ffb1f1e
XuhuiLee <lxh199108@gmail.com>
2019-02-12 20:57:39 +0800

    懒加载

--- src/main/webapp/js/image_server/page/list.js
+++ src/main/webapp/js/image_server/page/list.js
@@ -2,29 +2,40 @@
  * Created by lixuhui on 2019/2/12.
  */
 function listDir(dirName) {
-    $.ajax({
-        url: "/img/dir/" + dirName,
-        type: 'GET',
-        dataType: "json",
-        success: function (data) {
-            if (data.code == 0) {
-                showPics(dirName, data.data);
-            } else {
-                alert("code:" + data.code + ", msg:" + data.msg);
+    var $ul = $('#' + dirName);
+    if ($ul.html().trim().length == 0) {
+        $.ajax({
+            url: "/img/dir/" + dirName,
+            type: 'GET',
+            dataType: "json",
+            success: function (data) {
+                if (data.code == 0) {
+                    showPics(dirName, data.data);
+                } else {
+                    alert("code:" + data.code + ", msg:" + data.msg);
+                }
+            },
+            error: function(){
+                alert("系统异常");
             }
-        },
-        error: function(){
-            alert("系统异常");
+        });
+    } else {
+        if ($ul.is(":hidden")) {
+            $ul.show();
+        } else {
+            $ul.hide();
         }
-    });
+    }
 }
 
 function showPics(dirName, data) {
     var html = "";
+    var $ul = $('#' + dirName);
     for (var pic of data) {
-        html += '<li><a href="#">' +
+        html += '<li><a href="/img/pic/' + pic + '" target="_blank">' +
             '<img class="am-img-thumbnail am-img-bdrs" src="/img/pic/' + pic + '"/>' +
             '</a></li>';
     }
-    $('#' + dirName).html(html);
+    $ul.html(html);
+    $ul.show();
 }
